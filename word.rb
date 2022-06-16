b26ed9c958e19951bd5d78d1801e360f15ee05f3
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-16 18:13:14 +0530

    message

--- word.rb
+++ word.rb
@@ -14,12 +14,16 @@ def worddef(s)
     url = "https://fourtytwowords.herokuapp.com/word/#{w}/definitions?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
     response = HTTParty.get(url)
     pp=response.parsed_response
-    puts(pp)
+    a=[]
+    for i in 0..pp.length()-1
+        a.push(pp[i]["text"])
+    end
+    puts(a)
     return
 end
 def wordexample(s)
     if s==""
-    url = 'http://fourtytwowords.herokuapp.com/words/randomWord?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201'
+    url = "http://fourtytwowords.herokuapp.com/words/randomWord?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
     response = HTTParty.get(url)
     p=response.parsed_response
     w=p["word"]
@@ -31,9 +35,15 @@ def wordexample(s)
     #puts("swarag reddy #{w}")
     url = "https://fourtytwowords.herokuapp.com/word/#{w}/examples?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
     response = HTTParty.get(url)
-    pp=response.parsed_response
-    puts(pp)
-    return 
+    pp=[]
+    pp.push(response.parsed_response)
+    a=[]
+    for i in 0..pp.length()-1
+        for j in 0..pp[i]["examples"].length()-1
+            a.push(pp[i]["examples"][j]["text"])
+        end
+    end
+    puts(a)
 end
 def synonyms(q,s)
     if s==""
@@ -48,60 +58,41 @@ def synonyms(q,s)
     end
     url = "https://fourtytwowords.herokuapp.com/word/#{w}/relatedWords?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
     response = HTTParty.get(url)
-    pp=response.parsed_response
-    u=pp.to_s
-    ss=[]
-    aa=[]
-    s=0
-    a=0
-    w1=0
-    w=""
-    for i in 0..u.length()-1
-        if u[i]=="}"
-            s=0
-            a=0
-            w1=0
-        end
-        if u[i]>="a" && u[i]<="z"
-            w=w+u[i]
-        else 
-            if w!=""
-                if w=="antonym"
-                    a=1
-                    s=0
-                elsif w=="synonym"
-                    s=1
-                    a=0
-                end
-                if w=="words"
-                    w1=1
-                end
-                if a==1 && w1==1 && w!="words"
-                    aa.push(w)
-                elsif s==1 && w1==1 &&w!="words"
-                    ss.push(w)
-                end
-
-            w=""
+    p=response.parsed_response
+    a=[]
+    s=[]
+    aa=0
+    ss=0
+    for i in 0..p.length()-1
+        p[i].each do |k,v|
+            if v=="synonym"
+                aa=0
+                ss=1
+            elsif v=="antonym"
+                aa=1
+                ss=0
+            elsif aa==1 
+                a.push(v)
+            elsif ss=1
+                s.push(v)
             end
+
         end
-    end
+    end     
     if q==2 || q==5
         puts("the synonyms of the word are")
-        if ss.length()==0
+        if s.length()==0
             puts("sorry! there are no synonyms for the word in the database")
         else
-            print(ss)
-            puts()
+           puts(s)
         end
     end
     if q==3 || q==5
-        if aa.length()==0
+        if a.length()==0
             puts("sorry! there are no antonyms for the word in the database")
         else
             puts("the antonyms of the word are")
-            print(aa)
-            puts()
+            puts(a)
         end
     end
 
@@ -150,9 +141,13 @@ while 1
         puts("you will get a random word and the entire dict of the word")
         fulldict()
     end
-
+    if q==6
+        puts("you can play a word game with us")
+        wordgame()
+    end
     if q==7
         break
     end
 end
+
 puts("end")
