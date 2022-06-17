cf9af85acb86bd5f101e8feb39e8c5165cbbb23c
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-17 16:47:34 +0530

    completed

--- word.rb
+++ word.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+$a=[]
 def worddef(s)
     if s==""
     url = 'http://fourtytwowords.herokuapp.com/words/randomWord?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201'
@@ -19,15 +20,18 @@ def worddef(s)
     a=[]
     for i in 0..pp.length()-1
         a.push(pp[i]["text"])
+        $a.push(pp[i]["text"])
     end
     rescue
     r=1
     end
+    if $a[0]!=1
     if r==0
     puts(a)
     else
         puts("sorry the word is not available in our database")
     end
+end
     return
 end
 def wordexample(s)
@@ -93,8 +97,10 @@ def synonyms(q,s)
                 ss=0
             elsif aa==1 
                 a.push(v)
+                $a.push(v)
             elsif ss=1
                 s.push(v)
+                $a.push(v)
             end
 
         end
@@ -102,6 +108,7 @@ def synonyms(q,s)
     rescue
     r=1
     end 
+    if $a[0]!=1
     if r==1
         puts("sorry! the word is not in our database")
     else
@@ -122,7 +129,7 @@ def synonyms(q,s)
         end
     end
 end
-
+end
 end
 def fulldict(s)
     if s==""
@@ -140,5 +147,86 @@ def fulldict(s)
     puts("the examples of the word are")
     wordexample(w)
     puts("the antonyms and synonyms of the word are")
-    synonyms(5,w)
+    synonyms("sa",w)
 end
+def c(s,i,v)
+    if i>=1
+        if s!=v
+        $a.push(s)
+        return
+        end
+    end
+    for j in i..s.length()-1
+        t=s.dup
+        q=t[i]
+        t[i]=t[j]
+        t[j]=q
+        c(t,i+1,v)
+        end
+    end
+def wordgame()
+    score=0
+    while 1
+    $a.clear()
+    $a[0]=1
+    u='http://fourtytwowords.herokuapp.com/words/randomWord?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201'
+    r=HTTParty.get(u)
+    w=r.parsed_response
+    w=w["word"]
+    worddef(w)
+    synonyms("sa",w)
+    c(w,0,w)
+    i=0
+    q=2
+    while i<$a.length()
+        if score<0
+            score=0
+        end
+        if q==2
+            puts("the following is the hint of the word that you need to guess(could be a definition,antonym,synonym,jumbled letters of the word:")
+            i=i+1
+            puts($a[i])
+        elsif q==3
+            break
+        end
+        puts("please enter your guess")
+        r=gets.chomp
+        if r==w
+            puts("congratulations! you have guessed correctly")
+            score=score+10
+            break
+    else
+        puts("you have entered incorectly")
+        score=score-2
+        puts("1:try again")
+        puts("2:take a hint")
+        puts("3:skip")
+        q=gets.chomp
+        q=q.to_i
+        if q==2
+            score=score-3
+        end
+        if q==3
+            score=score-4
+        end
+    end
+    end
+    puts("your current score is:")
+    puts(score)
+    puts("1:try again")
+    puts("2:end game")
+    q=gets.chomp
+    q=q.to_i
+    if q==2
+        puts("Thanks! for playing")
+        break
+    end
+    $a.clear()
+    $a[0]=0
+    end
+
+
+
+end
+
+    
