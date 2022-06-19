692beeef4080cbd9d01bdd021d4e23d816352c28
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-19 13:40:29 +0530

    get_word

---
+++ ace.rb
@@ -0,0 +1,11 @@
+require 'httparty'
+
+def new_word()
+  url = 'http://fourtytwowords.herokuapp.com/words/randomWord?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201'
+  response = HTTParty.get(url)
+  result = response.parsed_response
+  word = result['word']
+  puts('the word is :')
+  puts(word)
+  return word
+end
