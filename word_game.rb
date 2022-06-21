160fbe75fff84909182730767bd891b4535df978
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-21 12:02:23 +0530

    word_game

--- word_game.rb
+++ word_game.rb
@@ -1,15 +1,19 @@
 require 'httparty'
 def word_game()
+  score = [0]
   while 1
-    score = [0]
-    array = []
+    hint = []
     word = new_word
-    array = array + synonyms(word,'true')
-    combinations_of_the_word(word,0,word,array)
-    array = array + antonyms(word,'true')
-    array = array + word_def(word,'true')
-    game(array,score,word)
+    hint = hint + synonyms(word,'true')
+    combinations_of_the_word(word , 0, word, hint)
+    hint = hint + antonyms(word,'true')
+    hint = hint + word_def(word,'true')
+    hint = hint.shuffle
+    game(hint, score, word)
     puts("your current score is:")
+    if score[0] < 0
+      score[0] = 0
+    end
     puts(score[0])
     puts("1:play again")
     puts("2:end game")
@@ -20,26 +24,22 @@ def word_game()
     break
     end
   end
-  return
 end
 
-def game(array,score,word)
+def game(hint, score, word)
   index = -1
   choice = 2
-  while index<array.length()
-    if score[0]<0
-      score[0] = 0
-    end
+  while index < hint.length()
     if choice == 2
       puts("the following is the hint of the word that you need to guess(could be a definition,antonym,synonym,jumbled letters of the word:")
       index = index + 1
-      puts(array[index])
+      puts(hint[index])
     end
     puts("please enter your guess")
     guess = gets.chomp
     if guess == word
       puts("congratulations! you have guessed correctly")
-      score[0] = score[0]+10
+      score[0] = score[0] + 10
       return
     else
       choice = incorrect_choice(score)
@@ -48,40 +48,34 @@ def game(array,score,word)
       end
     end
   end
-  return
 end
 
 def incorrect_choice(score)
   puts("you have entered incorectly")
-  score[0] = score[0]-2
+  score[0] = score[0] - 2
   puts("1:try again")
   puts("2:take a hint")
   puts("3:skip")
-  choice1 = gets.chomp
-  choice1 = choice1.to_i
-  if choice1 == 2
-    score[0] = score[0]-3
-  elsif choice1 == 3
-    score[0] = score[0]-4
+  choice = gets.chomp.to_i
+  if choice == 2
+    score[0] = score[0] - 3
+  elsif choice == 3
+    score[0] = score[0] - 4
   end
-  if score[0]<0
-    score[0]=0
-  end
-  return choice1
+  choice
 end
 
-def combinations_of_the_word(str,index,word,array)
-  if index>=1
-    if str!=word
-      array.push(str)
-        return
+def combinations_of_the_word(str, index, word, hint)
+  if index >= 1
+    if str != word
+      hint.push(str)
     end
   end
-  for j in index..str.length()-1
+  for j in index..str.length() - 1
     temp_str = str.dup
     temp_var = temp_str[index]
     temp_str[index] = temp_str[j]
     temp_str[j] = temp_var
-    combinations_of_the_word(temp_str,index+1,word,array)
+    combinations_of_the_word(temp_str, index+1, word, hint)
   end
 end
