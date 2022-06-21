3bc4c0eb4b2ab37bbe3375574e3414ef94292835
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-21 12:02:54 +0530

    word_example

--- word_example.rb
+++ word_example.rb
@@ -6,20 +6,20 @@ def word_example(str)
     else
       word = str
     end
-    check = 0
+    if_word_exist = true
     begin
       result = get_example(word)
-      array = []
-      for i in 0..result.length()-1
-        for j in 0..result[i]["examples"].length()-1
-          array.push(result[i]["examples"][j]["text"])
+      examples = []
+      for i in 0..result.length() - 1
+        for j in 0..result[i]["examples"].length() - 1
+          examples.push(result[i]["examples"][j]["text"])
         end
       end
     rescue => error
-      check = 1
+      if_word_exist = false
     end
-    if check.zero?
-      puts(array)
+    if if_word_exist == true
+      puts(examples)
     else
       puts('sorry the word doesnot exist in our database')
     end
@@ -29,6 +29,5 @@ def get_example(word)
   url  = "https://fourtytwowords.herokuapp.com/word/#{word}/examples?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
   response = HTTParty.get(url)
   result = []
-  result.push(response.parsed_response)
-  return result 
+  return result.push(response.parsed_response) 
 end
