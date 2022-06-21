5e9098f42c466e2f7008e63732b9db01bcc77749
swaragreddy07 <swaragreddy07@gmail.com>
2022-06-21 12:01:51 +0530

    antonym.rb

--- antonym.rb
+++ antonym.rb
@@ -1,5 +1,5 @@
 require 'httparty'
-def antonyms(str,for_word_game)
+def antonyms(str, for_word_game = 'false')
   if str == ""
     word = new_word
     puts("the word is :#{word}")
@@ -7,40 +7,40 @@ def antonyms(str,for_word_game)
     word = str 
   end
   begin
-  check = 0
-  result = get_antonyms(word)
-  array = []
-  start = 0
-  for i in 0..result.length()-1
-    result[i].each do |key,value|
-      if value == "synonym"
-        break
-      end 
-      if value == "antonym"
-        start = 1
-      elsif start == 1 
-        value.each do |i|
-          array.push(i)
+    if_word_exist = true
+    result = get_antonyms(word)
+    antonym = []
+    start = 0
+    for i in 0..result.length()-1
+      result[i].each do |key, value|
+        if value == "synonym"
+          break
+        end 
+        if value == "antonym"
+          start = 1
+        elsif start == 1 
+          value.each do |i|
+            antonym.push(i)
+          end
         end
-      end
-   end
+     end
   end    
   rescue
-    check = 1
+    if_word_exist = false
   end 
   if for_word_game == 'true'
-    return array
+    return antonym
   else
-    result(array,check)
+    result(antonym, if_word_exist)
   end
 end
 
-def result(array,check)
-  if check == 1 || array.length() == 0
+def result(antonym, if_word_exist)
+  if if_word_exist == false || antonym.length() == 0
     puts('the antonyms of the word does not exist in our database')
   else
     puts('the antonyms of the word are:')
-    puts(array)
+    puts(antonym)
   end
   return
 end
@@ -48,6 +48,5 @@ end
 def get_antonyms(word)
   url = "https://fourtytwowords.herokuapp.com/word/#{word}/relatedWords?api_key=be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201"
   response = HTTParty.get(url)
-  result=response.parsed_response
-  return result
+  return result=response.parsed_response
 end
